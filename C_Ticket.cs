74d0675eafdd47fb4c7e6c19eae341d48b418f00
Kubaikin Dmitry <dimian150@gmail.com>
2020-09-22 18:56:54 +0300

    end

---
+++ C_Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket
+{
+    class C_Ticket
+    {
+        public bool happy(string ticket)
+        {
+            int num1 = 0, num2 = 0;
+
+            char[] arr = new char[7];
+            arr[6] = '\0';
+
+            arr = ticket.ToCharArray();
+
+            for (int i = 0; i < ticket.Length/2; i++)
+            {
+                num1 += Convert.ToInt32(arr[i]);
+                num2 += Convert.ToInt32(arr[i+3]);
+            }
+
+            if (num1==num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,64 @@ namespace Ticket
     {
         static void Main(string[] args)
         {
+            string ticket;
+            int num;
+            C_Ticket tick = new C_Ticket();
+            while (true)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите номер билета:");
+                        ticket = Console.ReadLine();
+
+                        if (ticket.Length<6||ticket.Length>6)
+                        {
+                            throw FormatDim();
+                        }
+
+                        num = Convert.ToInt32(ticket);
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("\nНеверный формат ввода. Введите число действительного типа");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    catch(NotImplementedException)
+                    {
+                        Console.WriteLine("Должно быть введенно 6 цифр!!!");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+
+                if (tick.happy(ticket))
+                {
+                    Console.WriteLine("Ваш билет счастливый");
+                }
+                else
+                {
+                    Console.WriteLine("Ваш билет не счастливый");
+                }
+
+                Console.WriteLine("Хотите повторить?");
+                Console.WriteLine("Нажмите 1");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num!=1)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+
+        private static Exception FormatDim()
+        {
+            throw new NotImplementedException();
         }
     }
 }
